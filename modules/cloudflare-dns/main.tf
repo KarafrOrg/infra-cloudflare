16a4daba9c04f6fe748fa87586b3171c04a926b2
Karafra <55988328+karafra@users.noreply.github.com>
2026-08-21 14:38:46 +0200

    Add tags

--- modules/cloudflare-dns/main.tf
+++ modules/cloudflare-dns/main.tf
@@ -37,6 +37,5 @@ resource "cloudflare_dns_record" "records" {
   content = each.value.content
   ttl     = try(each.value.ttl, 1)
   proxied = try(each.value.proxied, true)
-  tags    = ["terraform"]
   comment = "Managed by Terraform: https://github.com/KarafrOrg/infra-cloudflare"
 }
